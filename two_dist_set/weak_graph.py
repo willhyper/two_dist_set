b26b9ef1334e56847d60c81764c1cd16aab2198b
willhyper <willhyper@gmail.com>
2017-12-05 15:56:08 -0800

    fix bug: used has to be in _cumsum

--- two_dist_set/weak_graph.py
+++ two_dist_set/weak_graph.py
@@ -4,25 +4,26 @@ from collections import deque
 import numpy as np
 import itertools
 
+
 def generate(A, v, k, l, u):
     q = deque()
     q.append(A)
 
     while q:
         simple_sym_graph = q.pop()
-        if len(simple_sym_graph) == v - 1: # data structure property. when met, graph is complete
+        if len(simple_sym_graph) == v - 1:  # data structure property. when met, graph is complete
             # print(len(q), simple_sym_graph)
-            yield simple_sym_graph # weak
+            yield simple_sym_graph  # weak
         else:
-            weak_candidates = sorted(_generate(simple_sym_graph,v,k,l,u))
+            weak_candidates = sorted(_generate(simple_sym_graph, v, k, l, u))
             for c in weak_candidates:
-                q.append( simple_sym_graph + (c,))
+                q.append(simple_sym_graph + (c,))
 
 
 def _cumsum(As, iteration):
     cumsum = np.zeros(iteration, dtype=np.int)
     row = len(As)
-    for digit in range(iteration-1, -1, -1):
+    for digit in range(iteration - 1, -1, -1):
         aa, As = zip(*[(a % 2, a >> 1) for a in As])
         cumsum[digit] = sum(e << (row - s) for s, e in enumerate(aa))
 
@@ -30,7 +31,7 @@ def _cumsum(As, iteration):
     return cumsum, used
 
 
-def _generate(As,v,k,l,u):
+def _generate(As, v, k, l, u):
     # As = (240, 14, 56)
 
     # A = ((1, 1, 1, 1, 0, 0, 0, 0),    <= 8 = 9 - 1 = v - 1
@@ -41,30 +42,25 @@ def _generate(As,v,k,l,u):
     #                ^  ^  ^  ^  ^
     #      cumsum = 10  6  4  4  0      <=len(cumsum) = 5 = 6 - 1 = col - 1
 
-    row = len(As)
-
     cumsum_len = v - len(As) - 1
-    cumsum, used = _cumsum(As, cumsum_len) # need a cumsum array whose length is col - 1
+    cumsum, used = _cumsum(As, cumsum_len)  # need a cumsum array whose length is col - 1
+    remain = k - used
 
-    if used > k:
+    if remain < 0:
         return
 
-    def is_maintain_reverse_sorted_when_adding_1_at(indices):
+    for indices in itertools.combinations(range(cumsum_len), remain):
         for i in indices:
             if i == 0:
                 continue
             # elif cumsum[i] +1 > cumsum[i-1] + (1 if i-1 in indices else 0):
             elif cumsum[i] > (cumsum[i - 1] if i - 1 in indices else cumsum[i - 1] - 1):
-                return False
-
-        return True
-
-    for indices in itertools.combinations(range(cumsum_len), k - used):
-        if is_maintain_reverse_sorted_when_adding_1_at(indices):
-            # if 0, add 16
-            # if 1, add 8
-            # if 2, add 4
-            # if 3, add 2
-            # if 4, add 1
+                break
+        else:
+            # if indices includes 0, add 16
+            # if indices includes 1, add 8
+            # if indices includes 2, add 4
+            # if indices includes 3, add 2
+            # if indices includes 4, add 1
             candidate = sum(1 << (cumsum_len - i - 1) for i in indices)
             yield candidate
