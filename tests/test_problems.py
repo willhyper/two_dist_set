6d7ea1f02634cef2af9203fdaa6ffccf4d722696
willhyper <willhyper@gmail.com>
2017-12-05 16:08:45 -0800

    fail test if 'actual' returns nothing

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -10,7 +10,10 @@ import pytest
 
 @pytest.mark.parametrize('v,k,l,u,expected', problems)
 def test_srg(v, k, l, u, expected):
-    seed = strong_graph.generate_seed(v,k,l,u)
+    seed = strong_graph.generate_seed(v, k, l, u)
 
-    for g, e in zip(strong_graph.generate(seed,v,k,l,u), expected):
+    actual = list(strong_graph.generate(seed, v, k, l, u))
+    assert len(actual) == len(expected)
+
+    for g, e in zip(actual, expected):
         assert np.array_equal(g, e)
