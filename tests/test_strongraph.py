188411fe51ee463aebf1ce3e94f154886d76d46a
willhyper <willhyper@gmail.com>
2017-12-08 12:32:57 -0800

    check eigenvalues and determinant

---
+++ tests/test_strongraph.py
@@ -0,0 +1,55 @@
+__author__ = 'chaoweichen'
+
+from two_dist_set import strong_graph
+from two_dist_set.problem_database import *
+
+import numpy as np
+from collections import Counter
+
+import pytest
+
+problems = []
+problems.append(problem_4_2_0_2)
+problems.append(problem_5_2_0_1)
+problems.append(problem_6_3_0_3)
+problems.append(problem_6_4_2_4)
+problems.append(problem_9_4_1_2)
+problems.append(problem_10_3_0_1)
+problems.append(problem_10_6_3_4)
+problems.append(problem_12_6_0_6)
+problems.append(problem_13_6_2_3)
+problems.append(problem_15_8_4_4)
+problems.append(problem_16_5_0_2)
+problems.append(problem_16_6_2_2)
+problems.append(problem_16_9_4_6)
+problems.append(problem_16_10_6_6)
+problems.append(problem_17_8_3_4)
+# problems.append()
+# problems.append()
+# problems.append(problem_21_10_5_4)
+@pytest.mark.parametrize('v,k,l,u, expected', problems)
+def test_strong(v, k, l, u, expected):
+
+    not_conference_graph = strong_graph.conference(v,k,l,u) != 0
+
+    em_expected = {e: m for e, m in strong_graph.eig(v, k, l, u)}
+
+    det_expected = strong_graph.determinant(v, k, l, u)
+    print('expected determinant', det_expected)
+    print('expected (eigenvalue, multiplicity)', em_expected)
+
+    seed = strong_graph.generate_seed(v, k, l, u)
+    for mat in strong_graph.generate(seed, v, k, l, u):
+        eigval, eigvec = np.linalg.eig(mat)
+
+        eigval = tuple(int(round(x)) for x in eigval) if not_conference_graph else eigval
+
+        det = 1
+        for e in eigval: det *= e
+        det = int(round(det))
+        assert det == det_expected, "determinant disagree"
+
+        c = Counter(eigval)
+        em_actual = { k : c.get(k) for k in sorted(c.keys(), reverse=True)}
+
+        print('actual   (eigenvalue, multiplicity)', em_actual)
--- two_dist_set/strong_graph.py
+++ two_dist_set/strong_graph.py
@@ -4,12 +4,44 @@ from . import representation
 from . import weak_graph
 from collections import deque
 
-def check_problem(v, k, l, u):
-    return (v - k - 1) * u == k * (k - l - 1)
-
+def assert_arg(v, k, l, u):
+    assert (v - k - 1) * u == k * (k - l - 1), f'{(v,k,l,u)} is not a strongly regular graph problem.'
 
 def generate_seed(v, k, l, u):
     return 2 ** (v - 1) - 2 ** (v - k - 1),  # tuple of numbers
+
+def assert_strong(mat, v, k, l, u):
+    assert_arg(v, k, l, u)
+
+    I = np.identity(v, dtype=np.int)
+    J = np.ones((v, v), dtype=np.int)
+    const = (k-u)*I + u*J
+    assert np.array_equal(mat@mat - (l-u)*mat , const)
+
+    det = np.rint(np.linalg.det(mat))
+    assert det == determinant(v,k,l,u)
+
+def conference(v,k,l,u):
+    return 2*k + (v-1)*(l-u)
+
+def eig(v,k,l,u):
+    conf = conference(v,k,l,u) # if conference graph, conf == 0, so D becomes non-integer
+
+    D = np.sqrt((l-u)**2 + 4*(k-u))
+    D = int(D) if conf!=0 else D
+
+    eig = k, ((l-u) + D)/2, ((l-u) - D)/2
+    eig = tuple(int(x) if conf !=0 else x for x in eig) # if not conference graph, eigvalues are integer
+    mul = 1, int(((v-1) - conf/D)/2), int(((v-1) + conf/D)/2) # multiplicity is always integer
+
+    return tuple(zip(eig, mul))
+
+def determinant(v,k,l,u):
+    prod = 1
+    for e, m in eig(v,k,l,u):
+        prod *= e**m
+
+    return int(round(prod))
 
 
 def generate(seed, v, k, l, u):
