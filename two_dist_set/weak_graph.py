fd1ee2029f8405c2880fd77141940f2e1c499e35
willhyper <willhyper@gmail.com>
2017-12-15 23:32:34 -0800

    optmize weak_graph a bit

--- two_dist_set/weak_graph.py
+++ two_dist_set/weak_graph.py
@@ -12,7 +12,7 @@ def generate(s: SRG):
     #             ^
     #      used = 2
     #                ^  ^  ^  ^  ^
-    #      cumsum = 10  6  4  4  0      <=len(cumsum) = 5 = 6 - 1 = col - 1
+    #      enc    = 10  6  4  4  0         <=len(enc) = 5 = 6 - 1 = col - 1
 
     dec = s._encoded[s.state - 1]
     used = 0
@@ -21,21 +21,16 @@ def generate(s: SRG):
         dec >>= 1
 
     remain = s.k - used
-
-    if remain < 0:
-        return
-
     unknown_len = s.v - 1 - s.state  # 9 - 1 - 3 = 5
-    unknown = s._encoded[-unknown_len:]
+    enc = s._encoded[-unknown_len:]
 
     for indices in itertools.combinations(range(unknown_len), remain):
-        for i in indices:
-            if i == 0:
-                continue
-            # elif unknown[i] +1 > unknown[i-1] + (1 if i-1 in indices else 0):
-            elif unknown[i] > (unknown[i - 1] if i - 1 in indices else unknown[i - 1] - 1):
+        start_index = 1 if 0 in indices else 0 # 0 appears up to once. if appearing, it is always at index 0
+        for i in indices[start_index:]:
+            # enc[i] +1 > enc[i-1] + (1 if i-1 in indices else 0):
+            if enc[i] > (enc[i - 1] if i - 1 in indices else enc[i - 1] - 1):
                 break
         else:
             binn = np.zeros(unknown_len, dtype=np.int)
-            for i in indices: binn[i] = 1
+            for i in indices: binn[i] = 1 # indices could be[()]
             yield binn
