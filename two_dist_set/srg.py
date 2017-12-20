41848843ef8d1458d0dca419f83800fe21e337fd
willhyper <willhyper@gmail.com>
2017-12-20 00:48:51 -0800

    add useful methods and properties to SRG class

--- two_dist_set/srg.py
+++ two_dist_set/srg.py
@@ -5,19 +5,19 @@ import numpy as np
 class SRG:
     def __init__(self, v, k, l, u):
         self.v, self.k, self.l, self.u = v, k, l, u
-        self._state = 0
+        self._ri = 0
         self._encoded = np.zeros(v - 1, dtype=np.int)
 
     def add(self, row):
-        assert self._state + len(row) == self.v - 1, "length of row mismatches status"
+        assert self._ri + len(row) == self.v - 1, "length of row mismatches status"
 
-        self._encoded[self._state:] += row
-        self._encoded[self._state + 1:] <<= 1
-        self._state += 1
+        self._encoded[self._ri:] += row
+        self._encoded[self._ri + 1:] <<= 1
+        self._ri += 1
 
     @property
     def state(self):
-        return self._state
+        return self._ri
 
     @property
     def encoded_representation(self):
@@ -25,29 +25,45 @@ class SRG:
 
     @property
     def unknown_len_of_current_row(self):
-        return self.v - 1 - self._state  # 9 - 1 - 3 = 5
+        return self.v - 1 - self._ri  # 9 - 1 - 3 = 5 # 1 is the '0' is the diagonal
 
     @property
     def used_k_of_current_row(self):
-        dec = self._encoded[self._state - 1]
+        dec = self._encoded[self._ri - 1]
         used = 0
         while dec > 0:
             used += dec % 2
             dec >>= 1
-        return used
+        return used # = sum(self.known_partial_vector_of_current_row)
+
+    @property
+    def known_partial_vector_of_current_row(self):
+        dec = self._encoded[self._ri - 1]
+        decoded = list()
+        while dec > 0:
+            e = dec % 2
+            dec >>= 1
+            decoded.append(e)
+        return np.array(decoded[::-1], dtype=np.int)
+
+    def known_inner_prod_with_row(self, row):
+        mat = self.to_matrix_essential()
+        vec = mat[row, : self._ri]
+        known = self.known_partial_vector_of_current_row
+        return vec.dot(known)
 
     def copy(self):
         cp = SRG(self.v, self.k, self.l, self.u)
         cp._encoded = np.copy(self._encoded)
-        cp._state = self._state
+        cp._ri = self._ri
         return cp
 
     def __repr__(self):
-        return f'SRG({self._state} : {self._encoded})'
+        return f'SRG({self._ri} : {self._encoded})'
 
     def to_matrix(self):
         enc = self._encoded.copy()
-        i = self.state
+        i = self._ri
         mat = np.zeros((self.v, self.v), dtype=np.int)
         while i > 0:
             # decoding
@@ -63,9 +79,15 @@ class SRG:
         return mat
 
     def to_matrix_essential(self):
-
         enc = self._encoded.copy()
-        R, C, i = self.state, self.v, self.state
+        R, C, i = self._ri, self.v, self._ri
+        mat = SRG._decode(enc, R, C, i)
+
+        return mat
+
+    @classmethod
+    def _decode(cls, enc, R, C, i):
+
         mat = np.zeros((R, C), dtype=np.int)
         while i > 0:
             # decoding
@@ -89,3 +111,24 @@ class SRG:
             s.add(row)
 
         return s
+
+    def current_question(self):
+
+        print(f'(v, k, l, u) = {self.v, self.k, self.l, self.u}')
+
+        # construct a string representing the known part of the matrix
+        mat = self.to_matrix_essential()
+        strr = ''
+        R, C = mat.shape
+        for r in range(R):
+            row = mat[r,:]
+            strr += str(row)[1:-1] + '\n'
+        strr = strr[:-1] # remove the trailing \n
+        strr = strr.replace(' ', '')
+
+        current_known_partial_vec_str = str(self.known_partial_vector_of_current_row)[1:-1].replace(' ', '')
+
+        question = '?' * self.unknown_len_of_current_row
+
+        print(strr)
+        print(current_known_partial_vec_str + '0' + question)
