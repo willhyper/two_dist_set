4309301f5ac50de00590a2743ade5f2a2151bcc4
willhyper <willhyper@gmail.com>
2018-01-08 10:18:17 -0800

    add utility partition

---
+++ tests/test_util.py
@@ -0,0 +1,12 @@
+from two_dist_set import util
+import numpy as np
+
+
+def test_partition():
+    s = 6
+    bound = (5, 4)
+
+    expect = [(2, 4), (3, 3), (4, 2), (5, 1)]
+    actual = util.partition(s, bound)
+    for tuple_a, tuple_b in zip(actual, expect):
+        assert np.array_equal(tuple_a, tuple_b)
--- two_dist_set/util.py
+++ two_dist_set/util.py
@@ -40,3 +40,24 @@ def generate_seed(v: int, k: int, l: int, u: int):
 
     s.add(second_row)
     return s
+
+
+def partition(s: int, bounds: tuple) -> tuple:
+    assert s >= 0, "sum to be placed is required >= 0"
+
+    l = len(bounds)
+    assert l >= 1, "len(dict) is required >= 1"
+
+    bound, *others = bounds
+
+    if l > 1:
+
+        for v in range(min(bound, s) + 1):
+            rem = s - v
+
+            for t in partition(rem, others):
+                yield (v,) + t
+
+    else:
+        if s <= bound:
+            yield (s,)
