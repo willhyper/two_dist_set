350240339d0d8f78c0858812f46ebca03a54392a
willhyper <willhyper@gmail.com>
2018-01-16 15:48:55 -0800

    update cythonized.sh for new py files in

--- cythonize.sh
+++ cythonize.sh
@@ -3,7 +3,8 @@ cp two_dist_set/strong_graph.py two_dist_set/strong_graph.pyx
 cp two_dist_set/weak_graph.py two_dist_set/weak_graph.pyx
 cp two_dist_set/sort.py two_dist_set/sort.pyx
 cp two_dist_set/srg.py two_dist_set/srg.pyx
-
+cp two_dist_set/simplifier.py two_dist_set/simplifier.pyx
+cp two_dist_set/util.py two_dist_set/util.pyx
 
 python setup.py build_ext --inplace
 
