92787fa3dab1d79947c3cd79e7bed540380f800d
willhyper <willhyper@gmail.com>
2018-01-20 13:53:37 -0800

    use argparse to compute/list/draw srg

--- two_dist_set/util.py
+++ two_dist_set/util.py
@@ -4,6 +4,9 @@ import numpy as np
 from two_dist_set.srg import SRG
 from collections import defaultdict
 
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 def assert_arg(v: int, k: int, l: int, u: int):
     assert (v - k - 1) * u == k * (k - l - 1), f'{(v,k,l,u)} is not a strongly regular graph problem.'
@@ -106,3 +109,24 @@ def gauss_eliminate(A, b):
         Ab[row_rest, :] -= ratio @ v
 
     return Ab[:, :-1], Ab[:, -1]
+
+
+def draw(v, k, l, u, matrices):
+    for i, matrix in enumerate(matrices):
+
+        fig = plt.figure()
+
+        nodes = {n: str(n) for n in range(v)}
+        graph = nx.Graph()
+        graph.add_nodes_from(nodes.keys())
+
+        pos = nx.circular_layout(graph)
+        nx.draw_networkx_labels(graph, pos, nodes)
+
+        for r, c in zip(*matrix.nonzero()):
+            graph.add_edge(r, c)
+
+        nx.draw_circular(graph)
+
+        plt.axis('equal')
+        fig.savefig(f'srg_{v}_{k}_{l}_{u}_{i}.png')
