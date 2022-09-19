71a9b97b6cca10439bf9a45823c0539909486f4c
willhyper <willhyper@gmail.com>
2022-09-18 23:36:13 -0700

    -import argparse

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ complete enumeration to construct strongly regular graph.
 
 example usage:
 python -m two_dist_set 13 6 2 3
+python -m two_dist_set.database
+python -m two_dist_set.database list
+python -m two_dist_set.database list 13 6 2 3
+python -m two_dist_set.database draw 13 6 2 3
--- two_dist_set/__main__.py
+++ two_dist_set/__main__.py
@@ -1,61 +1,34 @@
 #!python
 #cython: language_level=3
-'''
-strongly regular graph generator
-http://www.win.tue.nl/~aeb/graphs/srg/srgtab.html
-http://www.maths.gla.ac.uk/~es/srgraphs.php
-
-65 32 15 16
-
-'''
 from two_dist_set import util, strong_graph
-from two_dist_set import database as db
 from pprint import pprint
 import time
+import sys
 
-import argparse
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Compute and draw strongly regular graphes (SRG).')
-    parser.add_argument('-p', metavar=('v', 'k', 'l', 'u'), type=int, nargs=4,
-                        help='4 integers representing v, k, l, u, defining a SRG')
-    parser.add_argument('-d', '--draw', action='store_true', help='output png of the SRG if defined in database.')
-    parser.add_argument('-l', '--list', action='store_true', help='list SRG in database')
-    args = parser.parse_args()
+    v, k, l, u = list(map(int, sys.argv[1:5]))
+    util.assert_arg(v, k, l, u)
+    seed = util.generate_seed(v, k, l, u)
 
+    srg_solved = []
+    start = time.time()
+    start_dummy = start
+    for s in strong_graph.solve(seed):
+        now = time.time()
+        elapsed_s, start = now - start, now
+        mat = s.to_matrix()
+        pprint(mat)
+        print(f'elapsed {elapsed_s} s')
+        srg_solved.append(s)
 
-    if args.list:
-        pprint(db.list_problems())
-    elif args.draw:
-        print('draw', args.p)
-        v, k, l, u = args.p
-        util.assert_arg(v, k, l, u)
-        matricies = db.get_solutions(v, k, l, u)
-        util.draw(v,k,l,u, matricies)
-    else:
+    elapsed_total = time.time() - start_dummy
 
-        v, k, l, u = args.p
-        util.assert_arg(v, k, l, u)
-        seed = util.generate_seed(v, k, l, u)
+    print('sorted matrix')
+    srg_solved.sort()
 
-        srg_solved = []
-        start = time.time()
-        start_dummy = start
-        for s in strong_graph.solve(seed):
-            now = time.time()
-            elapsed_s, start = now - start, now
-            mat = s.to_matrix()
-            pprint(mat)
-            print(f'elapsed {elapsed_s} s')
-            srg_solved.append(s)
+    matricies = [s.to_matrix() for s in srg_solved]
+    pprint(matricies)
 
-        elapsed_total = time.time() - start_dummy
-
-        print('sorted matrix')
-        srg_solved.sort()
-
-        matricies = [s.to_matrix() for s in srg_solved]
-        pprint(matricies)
-
-        print(f'totally elapsed {elapsed_total} s')
+    print(f'totally elapsed {elapsed_total} s')
